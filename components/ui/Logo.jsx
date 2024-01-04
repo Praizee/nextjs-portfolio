309dbb1376643d98cbdceffe75aa76370d3f913c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-04 23:59:36 +0100

    feat: new logo

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Link href="/">
-      <Image src="/logo.svg" width={54} height={54} priority alt="Logo" />
+      <Image src="/Logo.png" width={54} height={54} priority alt="Logo" />
     </Link>
   );
 };
