c21b193ebc84efbf1b16a71938e24f50f0f8fe28
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-05 00:15:33 +0100

    Update Logo.jsx

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Link href="/">
-      <Image src="/Logo.png" width={54} height={54} priority alt="Logo" />
+      <Image src="/Logo.png" width={65} height={65} priority alt="Logo" />
     </Link>
   );
 };
