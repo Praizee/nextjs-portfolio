e9d8bf2f7b4186a763ee0e1a69f5a83aef121dd0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-05 20:10:58 +0100

    feat: mobile navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,5 +1,29 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+import Socials from "./Socials";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { AlignJustify } from "lucide-react";
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+            <Socials containerStyles="flex gap-x-4" iconsStyles="text-2xl" />
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
