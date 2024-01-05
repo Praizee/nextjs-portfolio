df0484704f3ce068807b36e7acbf200663f3551c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-06 00:30:44 +0100

    feat: about section tabs updated

--- components/About.jsx
+++ components/About.jsx
@@ -24,14 +24,14 @@ const qualificationData = [
     title: "Education",
     data: [
       {
-        School: "Edexcel Uni",
-        Qualification: "Bachelor of Science",
-        Years: "2019 - 2022",
+        school: "Edexcel Uni",
+        qualification: "Bachelor of Science",
+        years: "2019 - 2022",
       },
       {
-        School: "Towemo College",
-        Qualification: "High School Diploma",
-        Years: "2013 - 2019",
+        school: "Towemo College",
+        qualification: "High School Diploma",
+        years: "2013 - 2019",
       },
     ],
   },
@@ -39,19 +39,19 @@ const qualificationData = [
     title: "Experience",
     data: [
       {
-        Company: "Voltwolf Limited",
-        Position: "React Developer",
-        Years: "2019 - 2022",
+        company: "Voltwolf Limited",
+        role: "React Developer",
+        years: "2019 - 2022",
       },
       {
-        Company: "Voltwolf Limited",
-        Position: "Frontend Intern",
-        Years: "2019 - 2022",
+        company: "Voltwolf Limited",
+        role: "Frontend Intern",
+        years: "2019 - 2022",
       },
       {
-        Company: "ICT NetWorld",
-        Position: "Frontend Intern",
-        Years: "2019 - 2022",
+        company: "ICT NetWorld",
+        role: "Frontend Intern",
+        years: "2019 - 2022",
       },
     ],
   },
@@ -131,17 +131,183 @@ const About = () => {
                 </TabsList>
                 {/* Tab Content */}
                 <div className="text-lg mt-12 xl:mt-8">
+                  {/* Personal Tab */}
                   <TabsContent value="personal">
-                    <h3>Specialist in quality product delivery</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipiscing elit.
-                      Dolor
-                    </p>
+                    <div className="text-center xl:text-left">
+                      <h3 className="h3 mb-4">Delivering quality products</h3>
+                      <p className="subtitle max-w-xl mx-auto xl:mx-0">
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit.
+                        Dolor
+                      </p>
+                      {/* Icons */}
+                      <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                        {infoData.map((item, index) => {
+                          return (
+                            <div
+                              className="flex items-center gap-x-4 mx-auto xl:mx-0"
+                              key={index}
+                            >
+                              <div className="text-primary ">{item.icon}</div>
+                              <div>{item.text}</div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                      {/* Languages */}
+                      <div className="flex flex-col gap-y-2">
+                        <div>Language Skills</div>
+                        <div className="border-b border-border"></div>
+                        <div>English, French, Yoruba</div>
+                      </div>
+                    </div>
                   </TabsContent>
+
+                  {/* Qualifications Tab */}
                   <TabsContent value="qualifications">
-                    Qualification Info
+                    <div className="">
+                      <h3 className="h3 mb-8 text-center xl:text-left">
+                        My Journey
+                      </h3>
+                      {/* Experience and Education */}
+                      <div className="grid md:grid-cols-2 gap-y-8">
+                        {/* Experience */}
+                        <div className="flex flex-col gap-y-6">
+                          <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                            <Briefcase />
+                            <h4>
+                              {getData(qualificationData, "Experience").title}
+                            </h4>
+                            {/* Experience List */}
+                            <div className="flex flex-col gap-y-8">
+                              {getData(
+                                qualificationData,
+                                "experience"
+                              ).data.map((item, index) => {
+                                const { company, role, years } = item;
+                                return (
+                                  <div
+                                    className="flex gap-x-8 group"
+                                    key={index}
+                                  >
+                                    <div className="h-[84px] w-px bg-border relative ml-2">
+                                      <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                    </div>
+                                    <div>
+                                      <div className="font-semibold text-xl leading-none mb-2">
+                                        {company}
+                                      </div>
+                                      <div className="text-lg leading-none text-muted-foreground mb-4">
+                                        {role}
+                                      </div>
+                                      <div className="text-base font-medium">
+                                        {years}
+                                      </div>
+                                    </div>
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        </div>
+
+                        {/* Education */}
+                        <div className="flex flex-col gap-y-6">
+                          <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                            <GraduationCap size={28} />
+                            <h4>
+                              {getData(qualificationData, "Education").title}
+                            </h4>
+                            {/* Education List */}
+                            <div className="flex flex-col gap-y-8">
+                              {getData(qualificationData, "Education").data.map(
+                                (item, index) => {
+                                  const { school, qualification, years } = item;
+                                  return (
+                                    <div
+                                      className="flex gap-x-8 group"
+                                      key={index}
+                                    >
+                                      <div className="h-[84px] w-px bg-border relative ml-2">
+                                        <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                      </div>
+                                      <div>
+                                        <div className="font-semibold text-xl leading-none mb-2">
+                                          {school}
+                                        </div>
+                                        <div className="text-lg leading-none text-muted-foreground mb-4">
+                                          {qualification}
+                                        </div>
+                                        <div className="text-base font-medium">
+                                          {years}
+                                        </div>
+                                      </div>
+                                    </div>
+                                  );
+                                }
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </TabsContent>
-                  <TabsContent value="skills">Skills Info</TabsContent>
+
+                  {/* Skills Tab */}
+                  <TabsContent value="skills">
+                    <div className="text-center xl:text-left">
+                      {/* <h4 className="h3 mb-8">Tools I use Daily</h4> */}
+                      <h4 className="h3 mb-8">Experience Tools</h4>
+                      <div className="mb-16">
+                        {/* Skills */}
+                        <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                        <div className="border-b border-border mb-4"></div>
+
+                        {/* Skill List */}
+                        <div>
+                          {getData(skillsData, "skills").data.map(
+                            (item, index) => {
+                              const { name } = item;
+                              return (
+                                <div
+                                  className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                                  key={index}
+                                >
+                                  <div className="font-medium">{name}</div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+
+                      {/* Tools */}
+                      <div>
+                        <h4 className="text-4xl font-semibold mb-2 xl:text-left">
+                          Tools
+                        </h4>
+                        <div className="border-b border-border mb-4"></div>
+                        {/* Tools List */}
+                        <div className="flex gap-x-8 justify-center xl:justify-start">
+                          {getData(skillsData, "tools").data.map(
+                            (item, index) => {
+                              const { imgPath } = item;
+                              return (
+                                <div>
+                                  <Image
+                                    src={imgPath}
+                                    width={48}
+                                    height={48}
+                                    alt=""
+                                    priority
+                                  />
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </TabsContent>
                 </div>
               </Tabs>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,12 @@ module.exports = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
     },
     extend: {
       colors: {
