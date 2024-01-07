aaa82b94995c0815750cfc22e8f47172b122509f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-07 16:20:26 +0100

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary"></span>
-              Hello
+              Hello 👋🏽
             </div>
             <h1 className="h1 max-w-md mb-8">Let's work together!</h1>
             <p className="subtitle max-w-[400px]">
