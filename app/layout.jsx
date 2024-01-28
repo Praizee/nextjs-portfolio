e9694db0c23dd4cfbec2eda9bfec0a1c34de38d6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-28 14:56:07 +0100

    chore: change default theme from 'light' to 'system'

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={outfit.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
           {children}
           <BackToTopButton />
