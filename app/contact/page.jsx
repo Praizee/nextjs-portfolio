b242e48c40064654fd97c59f5438c9f7c5f083fd
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-03-03 00:18:41 +0100

    chore: contact and projects page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,79 +1,13 @@
-import Form from "@/components/Form";
-import { MailIcon, HomeIcon, PhoneCall } from "lucide-react";
+import Contact from "@/components/contact/Contact";
+import React from "react";
 
 export const metadata = {
   title: "Contact",
-  description: "Contact Page",
+  description: "I'd love to hear from you!",
 };
 
-const Contact = () => {
-  return (
-    <section>
-      <div className="container mx-auto">
-        {/* Text & Image */}
-        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
-          {/* Text */}
-          <div className="flex flex-col justify-center">
-            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
-              <span className="w-[30px] h-[2px] bg-primary"></span>
-              Hello 👋🏽
-            </div>
-            <h1 className="h1 max-w-md mb-8">Let's work together!</h1>
-            <p className="subtitle max-w-[400px]">
-              Feel free to reach out to me for collaboration, inquiries, or just
-              to say hi. <br /> I look forward to hearing from you!
-            </p>
-          </div>
-          {/* Image */}
-          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
-          {/* bg-contact_illustration */}
-        </div>
-        <div>
-          {/* Info text & Form*/}
-          <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
-            {/* Info Text */}
-            <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
-              {/* Phone */}
-              <div className="flex items-center gap-x-8">
-                <PhoneCall size={18} className="text-primary" />
-                <div>
-                  <a
-                    href="https://wa.link/wk4gts"
-                    className="hover:text-primary transition duration-300"
-                  >
-                    +234 902 892 3853
-                  </a>
-                </div>
-              </div>
-              {/* Mail */}
-              <div className="flex items-center gap-x-8">
-                <MailIcon size={18} className="text-primary" />
-                <div>
-                  <a
-                    href="mailto:adeolaastephen@gmail.com"
-                    className="hover:text-primary transition duration-300"
-                  >
-                    adeolaastephen@gmail.com
-                  </a>
-                </div>
-              </div>
-              {/* Address */}
-              <div className="flex items-center gap-x-8">
-                <HomeIcon size={18} className="text-primary" />
-                <div>Lagos, Nigeria</div>
-              </div>
-            </div>
-
-            <Form />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+const page = () => {
+  return <Contact />;
 };
 
-export default Contact;
-
-// Contact Feel free to reach out to me for collaboration or inquiries. You
-//       can contact me via email at [YourEmail@example.com] or connect with me on
-//       [LinkedIn/Twitter/GitHub]. I look forward to hearing from you!
+export default page;
---
+++ components/projects/Projects.jsx
@@ -0,0 +1,127 @@
+"use client";
+
+import React, { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+  {
+    image: "/work/car-hub.png",
+    category: "next.js",
+    // gsap
+    name: "Car Hub - Car Showcase",
+    description:
+      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+    link: "https://car-showcase-app-seven.vercel.app/",
+    github: "https://github.com/Praizee/car-showcase-app",
+  },
+  {
+    image: "/work/financial-dashboard.png",
+    category: "next.js",
+    // gsap
+    name: "Financial Dashboard",
+    description:
+      "Effortlessly manage invoices and customers with our Financial Dashboard. Built on Next.js, it provides a seamless experience for creating, editing, and deleting invoices, enhancing your financial workflow",
+    link: "https://nextjs-dashboard-eight-zeta-22.vercel.app/",
+    github: "https://github.com/Praizee/nextjs-dashboard",
+  },
+  {
+    image: "/work/payment-wallet.png",
+    category: "react.js, Typescript",
+    name: "Payment Wallet",
+    description:
+      "Experience convenience at your fingertips with our E-Payment Wallet. Seamlessly buy data/airtime, pay bills, subscribe to cable services, and transfer funds—all within a secure and user-friendly platform. Simplify your financial transactions and enjoy the ease of managing your digital payments.",
+    link: "https://digital-payment-wallett.netlify.app/",
+    github: "https://github.com/Praizee/Digital-Payment-Wallett",
+  },
+  {
+    image: "/work/chat-app.png",
+    category: "react.js",
+    name: "Real-time Chat App",
+    description:
+      "Engage in seamless real-time conversations with our React-based chat app, powered by Appwrite for secure and efficient messaging.",
+    link: "https://realtime-chat-app-01.netlify.app/",
+    github: "#",
+  },
+  {
+    image: "/work/filmlane.png",
+    category: "html/css",
+    name: "Movie Website",
+    description:
+      "Experience a cinematic journey with our sleek and user-friendly movie website, featuring the latest releases and popular classics.",
+    link: "https://filmlane01.netlify.app/",
+    github: "#",
+  },
+  {
+    image: "/work/app-land.png",
+    category: "html/css/bootstrap",
+    name: "App Landing Page",
+    description:
+      "A stunning, visually appealing app landing page that converts visitors to users. Try it today!",
+    link: "https://praizee.github.io/App-Landing-Page/",
+    github: "https://github.com/Praizee/App-Landing-Page",
+  },
+];
+
+// Remove duplicated category
+// console.log(projectData.map((item) => item.category));
+const uniqueCategories = [
+  "all projects",
+  ...new Set(projectData.map((item) => item.category)),
+];
+
+const Projects = () => {
+  const [categories, SetCategories] = useState(uniqueCategories);
+  const [category, SetCategory] = useState("all projects");
+  // console.log(category);
+
+  const filteredProjects = projectData.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.category === category;
+  });
+
+  // console.log(filteredProjects);
+
+  return (
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My Projects
+        </h2>
+
+        {/* Tabs */}
+        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+          {/* Tab List */}
+          <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[740px] mb-12 mx-auto md:border dark:border-none">
+            {categories.map((category, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => SetCategory(category)}
+                  value={category}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto"
+                >
+                  {category}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+
+          {/* Tabs content */}
+          <div className="text-lg h-full xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
