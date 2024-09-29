793919205cea72a38a11f76c297ca18e44626dda
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-09-29 22:41:39 +0100

    chore: update project descriptions

--- components/projects/content.jsx
+++ components/projects/content.jsx
@@ -7,7 +7,7 @@ export const projectData = [
     category: "next.js",
     name: "Jumia Nigeria Online Shopping — Clone",
     description:
-      "PWA - Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "In-progress: A Progressive Web App (PWA) clone of Jumia Nigeria's online shopping platform. Mimicking key features like product listings, search, and cart functionalities using Next.js and Tailwind CSS.",
     link: "https://jumia-ng-clone.vercel.app/",
     github: "https://github.com/Praizee/jumia-ng-clone",
   },
@@ -17,7 +17,8 @@ export const projectData = [
     category: "next.js",
     name: "DevLinks - Links Sharing App",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      // "In-progress: A personalized Linktree-like app for developers, designed to showcase projects, repositories, and portfolios. Built with Next.js, Firebase, and Tailwind CSS.",
+      "In-progress: A personalized link-sharing app for developers to consolidate social media profiles and other external links. Built with Next.js, Firebase, and Tailwind CSS.",
     link: "https://devlinks-hng.vercel.app/",
     github: "https://github.com/Praizee/devlinks-hng/",
   },
@@ -27,7 +28,8 @@ export const projectData = [
     category: "react.js",
     name: "Traffic Tracker App",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "Front-end only: A traffic tracking app prototype built with React.js and Tailwind CSS. No APIs integrated yet, but the design and functionality are in place.",
+    // "A React-based app that helps users monitor traffic congestion in real-time using APIs and geolocation services. Features real-time data updates and a user-friendly UI.",
     link: "https://traffic-tracker-application.vercel.app/",
     github: "https://github.com/Praizee/Traffic-Tracker-Application",
   },
@@ -37,7 +39,7 @@ export const projectData = [
     category: "react.js",
     name: "Brainwave AI Chat App",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "A chatbot powered by AI with dynamic features like parallax scrolling and locked viewport. Built with React, Tailwind CSS, and Just Parallax for sleek user experience.",
     link: "https://brainwave-app-14.vercel.app/",
     github: "https://github.com/Praizee/brainwave-app",
   },
@@ -47,7 +49,7 @@ export const projectData = [
     category: "next.js",
     name: "Lazynerd Studios",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "A landing page for a creative agency. Features custom animations with Framer Motion, and a minimalistic design using Next.js and Tailwind CSS.",
     link: "https://the-lazynerd-studios.vercel.app/",
     github: "#",
   },
@@ -57,7 +59,7 @@ export const projectData = [
     category: "next.js",
     name: "Geegpay Analytics Dashboard",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "A comprehensive analytics dashboard for visualizing user and transaction data. Developed with Next.js and Chart.js for interactive data representation.",
     link: "https://geegpay-challenge.vercel.app/",
     github: "https://github.com/Praizee/geegpay-challenge",
   },
@@ -67,51 +69,36 @@ export const projectData = [
     category: "next.js",
     name: "ClearLink Landing Page",
     description:
-      "Lorem ipsum dolor sit amet rebum magna dolores blandit ea et et duo gubergren diam eirmod invidunt iusto et in",
+      "A responsive landing page for ClearLink. Built using Next.js and Tailwind CSS.",
     link: "https://talent-assessment.vercel.app/",
     github: "https://github.com/Praizee/talent-assessment",
   },
-  // {
-  //   // image: "/work/payment-wallet.png",
-  //   image:
-  //     "https://res.cloudinary.com/stephen1407/image/upload/v1709502844/iPhone-12-Mini-adorable-monstera-54dd37.netlify.app_fs8nd5.png",
-  //   category: "react.js",
-  //   // category: "react.js, Typescript",
-  //   name: "BIZPAY - Payment System",
-  //   description:
-  //     "Our application allows small business owners to easily create profiles, track payment transactions, and receive real-time alerts for unpaid invoices.",
-  //   link: "https://adorable-monstera-54dd37.netlify.app/",
-  //   github: "https://github.com/sparkle666/Bizpay",
-  // },
   {
-    // image: "/work/payment-wallet.png",
     image:
       "https://res.cloudinary.com/stephen1407/image/upload/v1709423770/iPhone-12-Mini-digital-payment-wallett.netlify.app_k5360v.png",
     category: "react.js",
-    // category: "react.js, Typescript",
     name: "Payment Wallet",
     description:
-      "Experience convenience at your fingertips with our E-Payment Wallet. Seamlessly buy data/airtime, pay bills, subscribe to cable services, and transfer funds—all within a secure and user-friendly platform. Simplify your financial transactions and enjoy the ease of managing your digital payments.",
+      "Front-end only: An e-payment wallet system that allows users to buy data, pay bills, and transfer funds securely. Built with React, Tailwind CSS, and Typescript.",
     link: "https://digital-payment-wallett.netlify.app/",
     github: "https://github.com/Praizee/Digital-Payment-Wallett",
   },
   {
-    // image: "/work/chat-app.png",
     image:
       "https://res.cloudinary.com/stephen1407/image/upload/v1709474222/iPhone-12-Mini-realtime-chat-app-01.netlify.app_nu2znv.png",
     category: "react.js",
     name: "Real-time Chat App",
     description:
-      "Engage in seamless real-time conversations with our React-based chat app, powered by Appwrite for secure and efficient messaging.",
+      "A chat application with real-time messaging and user authentication. Built using React and Appwrite for a smooth and secure communication experience.",
     link: "https://realtime-chat-app-01.netlify.app/",
     github: "#",
   },
   {
-    // image: "/work/filmlane.png",
     image: "",
     category: "GSAP",
-    name: "",
-    description: "",
+    name: "Coming soon...",
+    description:
+      "An experimental demo using GSAP for interactive animations and smooth transitions. Aiming to showcase complex UI interactions.",
     link: "",
     github: "#",
   },
@@ -124,7 +111,7 @@ export const projectData2 = [
       "https://res.cloudinary.com/stephen1407/image/upload/v1727635716/iPhone-12-Mini-jumia-ng-clone.vercel.app_xam14s.png",
     category: "next.js",
     name: "Jumia Nigeria Online Shopping — Clone",
-    description: "Technologies: Next.js, Tailwind CSS,",
+    description: "Technologies: Next.js, Tailwind CSS, PWA",
     link: "https://jumia-ng-clone.vercel.app/",
     github: "https://github.com/Praizee/jumia-ng-clone",
   },
@@ -133,7 +120,7 @@ export const projectData2 = [
       "https://res.cloudinary.com/stephen1407/image/upload/v1723893065/iPhone-12-Mini-devlinks-hng.vercel.app_sc5xfh.png",
     category: "next.js",
     name: "DevLinks - Links Sharing App",
-    description: "Technologies: Next.js, Firebase, Tailwind CSS,",
+    description: "Technologies: Next.js, Firebase, Tailwind CSS",
     link: "https://devlinks-hng.vercel.app/",
     github: "https://github.com/Praizee/devlinks-hng/",
   },
@@ -148,6 +135,33 @@ export const projectData2 = [
   },
   {
     image:
+      "https://res.cloudinary.com/stephen1407/image/upload/v1709514130/iPhone-12-Mini-the-lazynerd-studios.vercel.app_xrerhy.png",
+    category: "next.js",
+    name: "Lazynerd Studios",
+    description: "Technologies: Next.js, Tailwind CSS",
+    link: "https://the-lazynerd-studios.vercel.app/",
+    github: "#",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/stephen1407/image/upload/v1709423265/iPhone-12-Mini-geegpay-challenge.vercel.app_m04ry8.png",
+    category: "next.js",
+    name: "Geegpay Analytics Dashboard",
+    description: "Technologies: Next.js, Tailwind CSS, React Chart.js",
+    link: "https://geegpay-challenge.vercel.app/",
+    github: "https://github.com/Praizee/geegpay-challenge",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/stephen1407/image/upload/v1709423770/iPhone-12-Mini-digital-payment-wallett.netlify.app_k5360v.png",
+    category: "react.js",
+    name: "Payment Wallet",
+    description: "Technologies: React.js, Tailwind CSS, Typescript",
+    link: "https://digital-payment-wallett.netlify.app/",
+    github: "https://github.com/Praizee/Digital-Payment-Wallett",
+  },
+  {
+    image:
       "https://res.cloudinary.com/stephen1407/image/upload/v1709673890/iPhone-12-Mini-brainwave-app-14.vercel.app_zdahws.png",
     category: "react.js",
     name: "Brainwave AI Chat App",
@@ -158,44 +172,6 @@ export const projectData2 = [
   },
   {
     image:
-      "https://res.cloudinary.com/stephen1407/image/upload/v1709514130/iPhone-12-Mini-the-lazynerd-studios.vercel.app_xrerhy.png",
-    category: "next.js",
-    name: "Lazynerd Studios",
-    description: "Technologies: Next.js, Tailwind CSS,",
-    link: "https://the-lazynerd-studios.vercel.app/",
-    github: "#",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/stephen1407/image/upload/v1709423265/iPhone-12-Mini-geegpay-challenge.vercel.app_m04ry8.png",
-    category: "next.js",
-    name: "Geegpay Analytics Dashboard",
-    description: "Technologies: Next.js, Tailwind CSS, and React Chart-js",
-    link: "https://geegpay-challenge.vercel.app/",
-    github: "https://github.com/Praizee/geegpay-challenge",
-  },
-  // {
-  //   image:
-  //     "https://res.cloudinary.com/stephen1407/image/upload/v1709423286/iPhone-12-Mini-talent-assessment.vercel.app_nf4zxf.png",
-  //   category: "next.js",
-  //   name: "ClearLink Landing Page",
-  //   description: "Technologies: Next.js & Tailwind CSS",
-  //   link: "https://talent-assessment.vercel.app/",
-  //   github: "https://github.com/Praizee/talent-assessment",
-  // },
-  {
-    // image: "/work/payment-wallet.png",
-    image:
-      "https://res.cloudinary.com/stephen1407/image/upload/v1709423770/iPhone-12-Mini-digital-payment-wallett.netlify.app_k5360v.png",
-    category: "react.js",
-    name: "Payment Wallet",
-    description: "Technologies: React.js, Tailwind CSS, Typescript",
-    link: "https://digital-payment-wallett.netlify.app/",
-    github: "https://github.com/Praizee/Digital-Payment-Wallett",
-  },
-  {
-    // image: "/work/chat-app.png",
-    image:
       "https://res.cloudinary.com/stephen1407/image/upload/v1709474222/iPhone-12-Mini-realtime-chat-app-01.netlify.app_nu2znv.png",
     category: "react.js",
     name: "Real-time Chat App",
@@ -203,15 +179,4 @@ export const projectData2 = [
     link: "https://realtime-chat-app-01.netlify.app/",
     github: "#",
   },
-  // {
-  //   image: "/work/financial-dashboard.png",
-  //   image:
-  //     "https://res.cloudinary.com/stephen1407/image/upload/v1709423482/iPhone-12-Mini-nextjs-dashboard-eight-zeta-22.vercel.app_fenump.png",
-  //   category: "next.js",
-  //   name: "Financial Dashboard",
-  //   description:
-  //     "Technologies: Next.js, Tailwind CSS, and Vercel Postgres (Database)",
-  //   link: "https://nextjs-dashboard-eight-zeta-22.vercel.app/",
-  //   github: "https://github.com/Praizee/nextjs-dashboard",
-  // },
 ];
