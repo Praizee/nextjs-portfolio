3d6b6e5ea9da14475c677bff8bbe6dce6998c7f2
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-10-03 22:32:33 +0100

    feat: animated cursor

--- app/template.jsx
+++ app/template.jsx
@@ -2,6 +2,7 @@
 
 import { motion } from "framer-motion";
 import useScrollProgress from "@/hooks/useScrollProgress";
+import AnimatedCursor from "react-animated-cursor";
 
 const variants = {
   hidden: { opacity: 0 },
@@ -26,6 +27,37 @@ const Template = ({ children }) => {
         style={{ transform: `translateY(${completion - 100}%)` }}
         className="fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700"
       ></span>
+
+      <AnimatedCursor
+        // showSystemCursor={true}
+        innerSize={8}
+        outerSize={35}
+        color="30, 167, 255"
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "hsl(var(--primary))",
+        }}
+        outerStyle={{
+          border: "3px solid hsl(var(--primary))",
+        }}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+
       {/* <div className="h-[4000px]"></div> */}
     </>
   );
